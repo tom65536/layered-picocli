c86a2ac422902ce36ecca379b2bf6453398ff9ec
tom65536 <peregrinus@planet.ms>
2023-07-20 15:21:20 +0000

    fixes checkstyle violations

--- src/main/java/com/github/tom65536/picocli/layered/ArgNameDecoder.java
+++ src/main/java/com/github/tom65536/picocli/layered/ArgNameDecoder.java
@@ -10,24 +10,50 @@ import java.util.function.Function;
 import picocli.CommandLine.Model.ArgSpec;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Model.OptionSpec;
-import picocli.CommandLine.Model.PositionalParamSpec;
+// import picocli.CommandLine.Model.PositionalParamSpec;
 
  /**
-  * Transform argument specs to configuration property names.
+  * Transform argument specs to configuration property
+  * names.
   */
  public class ArgNameDecoder {
+   /**
+    * Eagerly instantiated default instance.
+    */
+   private static final ArgNameDecoder
+      DEFAULT_ARG_NAME_DECODER = new ArgNameDecoder();
+   /**
+    * The field transformer function used by this
+    * instance.
+    */
+   private final Function<String, String> keyTransform;
 
-   private static final ArgNameDecoder DEFAULT_ARG_NAME_DECODER = new ArgNameDecoder();
-   private final Function<String,String> keyTransform;
-
+   /**
+    * Return an instance of the default implementation.
+    *
+    * @return the default instance
+    */
    public static ArgNameDecoder getDefault() {
       return DEFAULT_ARG_NAME_DECODER;
    }
 
-   public ArgNameDecoder(final Function<CharSequence, String> aKeyTransformer) {
+   /**
+    * Initialize an argument name transformer with
+    * a transformation function.
+    *
+    * @param aKeyTransformer gets applied to each key
+    */
+   public ArgNameDecoder(
+     final Function<String, String> aKeyTransformer
+   ) {
       this.keyTransform = aKeyTransformer;
    }
 
+   /**
+    * Hide the default constructor.
+    *
+    * Use {@link #getDefault} instead.
+    */
    private ArgNameDecoder() {
       this(null);
    }
@@ -38,11 +64,12 @@ import picocli.CommandLine.Model.PositionalParamSpec;
     * Implementation taken from picocli-examples.
     *
     * @param prefixed the possibly prefixed string
-    * @return a string where all prefixed non-identifier characters are stripped
+    * @return a string where all prefixed non-identifier
+    *    characters are stripped
     */
    public static String stripPrefix(final String prefixed) {
-      for(int i = 0; i < prefixed.length(); ++i) {
-         if(!Character.isJavaIdentifierPart(prefixed.charAt(i))) {
+      for (int i = 0; i < prefixed.length(); ++i) {
+         if (!Character.isJavaIdentifierPart(prefixed.charAt(i))) {
             return prefixed.substring(i);
          }
       }
@@ -72,16 +99,22 @@ import picocli.CommandLine.Model.PositionalParamSpec;
     * @param commandSpec the (sub-)command of this setting
     * @return the parts of the qualified name.
     */
-   private String[] getKeySegments(final String key, final CommandSpec commandSpec) {
+   private String[] getKeySegments(
+      final String key,
+      final CommandSpec commandSpec) {
       final Stack<String> parts = new java.util.ArrayList<String>(5);
-      parts.push((keyTransform == null)? key : keyTransform(key));
+      parts.push((keyTransform == null) ? key : keyTransform(key));
 
-      for(CommandSpec cs = commandSpec; cs != null; cs = cs.parent()) {
-         parts.push((keyTransform == null)? cs.name() : keyTransform(cs.name()));
+      for (CommandSpec cs = commandSpec; cs != null; cs = cs.parent()) {
+         parts.push(
+            (keyTransform == null)
+            ? cs.name()
+            : keyTransform(cs.name())
+         );
       }
 
       String[] result = new String[parts.size()];
-      for(int i = 0; i < result.length; ++i) {
+      for (int i = 0; i < result.length; ++i) {
          result[i] = parts.pop();
       }
       return result;
